07c9ff313c6c317c35b6ddc5ffc772acc825c27b
Elen Kartina <affenmilchmann@gmail.com>
2026-04-22 22:33:25 +0300

    feat: frontend for v1_preview

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -15,3 +15,20 @@ pub fn encode(payload: &str, encoder: &str) -> String {
         Err(_) => "<failed>".to_string(),
     }
 }
+
+#[wasm_bindgen]
+pub fn decode(payload: &str, encoder: &str) -> String {
+    match encoders::process_payload(payload, encoder, true) {
+        Ok(processed_payload) => processed_payload,
+        Err(_) => "<failed>".to_string(),
+    }
+}
+
+#[wasm_bindgen]
+pub fn list_encoders() -> Vec<String> {
+    let mut encoders = Vec::new();
+    for encoder in encoders::NAMED_ENCODERS.keys() {
+        encoders.push(encoder.to_string());
+    }
+    encoders
+}
